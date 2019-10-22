8b335e72074666b1b55808afd5b983ebdbddbcbe
Sergey Chernyshev <serge.chernyshev@gmail.com>
2019-10-23 01:36:25 +0300

    local deployment
    Signed-off-by: Sergey Chernyshev <serge.chernyshev@gmail.com>

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 docker-compose down --rmi local --volumes
 ./gradlew killCordaProcesses clean assemble
 ./gradlew deployNodes
-./gradlew runNodes
-sleep 30
 docker-compose up -d agent94 agent95 agent96 agentInitiator
-docker-compose build tcweb mfweb
+docker-compose build tccorda mfcorda notary tcweb mfweb
+docker-compose up -d tccorda mfcorda notary
+sleep 60
 docker-compose up -d tcweb mfweb
